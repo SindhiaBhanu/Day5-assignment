2ec634dcd42f2b51624d7aa2d449bac93967911d
SindhiaBhanu <sindhiabhanu518@gmail.com>
2020-07-13 00:47:24 +0530

    added code to find different unit conversions

--- day5Programs.sh
+++ day5Programs.sh
@@ -1,9 +1,21 @@
 #!/bin/bash -x
-value1=${RANDOM:0:2}
-value2=${RANDOM:0:2}
-value3=${RANDOM:0:2}
-value4=${RANDOM:0:2}
-value5=${RANDOM:0:2}
-sum=$(( $value1+$value2+$value3+$value4+$value5 ))
-average=`expr $sum/5 | bc -l`
 
+#converting 42 inch into feet
+measureInFeet=`expr 42/12 | bc -l`
+echo $measureInFeet
+
+#calculating area of rectangle in feets
+rectangleLength=60
+rectangleWidth=40
+plotAreaInFeet=$(( 60*40 ))
+
+#coverting plot area into meters from Feet
+plotAreaInMeters=`expr $plotAreaInFeet\*0.3048 | bc -l`
+echo $plotAreaInMeters
+
+#coverting plot area into Acers from Meters
+plotAreaInAcers=`expr $plotAreaInMeters\*0.000247 | bc -l`
+
+#calculating area of 25 such plots in Acers
+TwentyFiveplotsAreaInAcers=`expr 25\*$plotAreaInAcers | bc -l`
+echo $TwentyFiveplotsAreaInAcers
