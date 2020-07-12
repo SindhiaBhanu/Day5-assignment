620a8d607ccfc3944cb499da937f26b3b52e13a8
SindhiaBhanu <sindhiabhanu518@gmail.com>
2020-07-13 01:39:04 +0530

    added code to read number and display in words using case

--- day5Programs.sh
+++ day5Programs.sh
@@ -1,53 +1,27 @@
 #!/bin/bash -x
-read -p "Enter the first number" a
-read -p "Enter the second number" b
-read -p "Enter the third number" c
-exp1=`expr $a+$b\*$c | bc -l`
-exp2=`expr $a%$b+$c | bc -l`
-exp3=`expr $c+$a/$b | bc -l`
-exp4=`expr $a\*$b+$c | bc -l`
-echo $exp1 $exp2 $exp3 $exp4
-condition1=`echo "$exp1>$exp2" | bc -l`
-if [ $condition1 -eq 1 ]
-then
-        max=$exp1
-else
-        max=$exp2
-fi
-condition2=`echo "$max>$exp3" | bc -l`
-if [ $condition2 -eq 1 ]
-then
-        max=$max
-else
-        max=$exp3
-fi
-condition3=`echo "$max>$exp4" | bc -l`
-if [ $condition3 -eq 1 ]
-then
-        max=$max
-else
-        max=$exp4
-fi
-echo "the maximum number is $max”
-condition1=`echo "$exp1<$exp2" | bc -l`
-if [ $condition1 -eq 1 ]
-then
-        min=$exp1
-else
-        min=$exp2
-fi
-condition2=`echo "$min<$exp3" | bc -l`
-if [ $condition2 -eq 1 ]
-then
-        min=$min
-else
-        min=$exp3
-fi
-condition3=`echo "$min<$exp4" | bc -l`
-if [ $condition3 -eq 1 ]
-then
-        min=$min
-else
-        min=$exp4
-fi
-echo "the minimum number is $min"
+read -p "Enter the number : " number
+case $number in
+        "0" )
+                echo "ZERO";;
+        "1" )
+                echo "ONE";;
+        "2" )
+                echo "TWO";;
+        "3" )
+                echo "THREE";;
+        "4" )
+                echo "FOUR";;
+        "5" )
+                echo "FIVE";;
+        "6" )
+                echo "SIX";;
+        "7" )
+                echo "SEVEN";;
+        "8" )
+                echo "EIGHT";;
+        "9" )
+                echo "NINE";;
+        * )
+                echo "Enter a valid number";;
+esac
+
