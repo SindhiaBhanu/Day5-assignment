1561a376d5bc4a555fddfcbfe2fafdc70570b8b6
SindhiaBhanu <sindhiabhanu518@gmail.com>
2020-07-13 01:34:35 +0530

    added code to read numbers and arithematic operation and find min and max

--- day5Programs.sh
+++ day5Programs.sh
@@ -1,24 +1,53 @@
 #!/bin/bash -x
-read -p "Enter a  number :" number
-if [ $number -eq "1" ]
+read -p "Enter the first number" a
+read -p "Enter the second number" b
+read -p "Enter the third number" c
+exp1=`expr $a+$b\*$c | bc -l`
+exp2=`expr $a%$b+$c | bc -l`
+exp3=`expr $c+$a/$b | bc -l`
+exp4=`expr $a\*$b+$c | bc -l`
+echo $exp1 $exp2 $exp3 $exp4
+condition1=`echo "$exp1>$exp2" | bc -l`
+if [ $condition1 -eq 1 ]
 then
-        echo "UNIT"
-elif [ $number -eq "10" ]
-then
-        echo "TEN"
-
-elif [ $number -eq "100" ]
-then
-        echo "HUNDRED"
-elif [ $number -eq "1000" ]
-then
-        echo "THOUSAND"
-elif [ $number -eq "10000" ]
-then
-        echo "TEN THOUSAND"
-elif [ $number -eq “100000” ]
-then
-        echo “LAKH”
+        max=$exp1
 else
-        echo "Enter a valid number"
+        max=$exp2
 fi
+condition2=`echo "$max>$exp3" | bc -l`
+if [ $condition2 -eq 1 ]
+then
+        max=$max
+else
+        max=$exp3
+fi
+condition3=`echo "$max>$exp4" | bc -l`
+if [ $condition3 -eq 1 ]
+then
+        max=$max
+else
+        max=$exp4
+fi
+echo "the maximum number is $max”
+condition1=`echo "$exp1<$exp2" | bc -l`
+if [ $condition1 -eq 1 ]
+then
+        min=$exp1
+else
+        min=$exp2
+fi
+condition2=`echo "$min<$exp3" | bc -l`
+if [ $condition2 -eq 1 ]
+then
+        min=$min
+else
+        min=$exp3
+fi
+condition3=`echo "$min<$exp4" | bc -l`
+if [ $condition3 -eq 1 ]
+then
+        min=$min
+else
+        min=$exp4
+fi
+echo "the minimum number is $min"
