e7a096a84fe0539ca7ef24a91289ef39f7559607
SindhiaBhanu <sindhiabhanu518@gmail.com>
2020-07-13 00:44:31 +0530

    added code to get 5 random digits and to find their sum and average

--- day5Programs.sh
+++ day5Programs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -x
-value1=$((RANDOM%6+1))
-echo "The first random number is : $value1"
-value2=$((RANDOM%6+1))
-echo "The second random number is : $value2"
-sum=$(( $value1 + $value2 ))
-echo $sum
+value1=${RANDOM:0:2}
+value2=${RANDOM:0:2}
+value3=${RANDOM:0:2}
+value4=${RANDOM:0:2}
+value5=${RANDOM:0:2}
+sum=$(( $value1+$value2+$value3+$value4+$value5 ))
+average=`expr $sum/5 | bc -l`
+
