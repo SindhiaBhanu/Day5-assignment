a66dc4064ad163a00666f6c4182eae59ba8e5c76
SindhiaBhanu <sindhiabhanu518@gmail.com>
2020-07-13 00:38:27 +0530

    added code to add two random dice numbers from 1 to 6

--- day5Programs.sh
+++ day5Programs.sh
@@ -1,1 +1,7 @@
-$ echo $(( $RANDOM % 6 + 1 ))
+#!/bin/bash -x
+value1=$((RANDOM%6+1))
+echo "The first random number is : $value1"
+value2=$((RANDOM%6+1))
+echo "The second random number is : $value2"
+sum=$(( $value1 + $value2 ))
+echo $sum
